e286824bd8d1b09a630d1c313df8e5170a6db8c9
maicolkutchoukov <maicol.j.95@gmail.com>
2024-05-27 09:37:57 +0200

    Create event class

--- src/org/events/Event.java
+++ src/org/events/Event.java
@@ -1,2 +1,57 @@
-package org.events;public class Event {
+package org.events;
+
+import java.time.LocalDate;
+
+public class Event {
+    private String title;
+    private LocalDate date;
+    private int totalPlaces;
+    private int reservedSeats;
+
+    public Event(String title, LocalDate date, int totalPlaces) {
+        dateValidation();
+        checkSeats();
+        this.title = title;
+        this.date = date;
+        this.totalPlaces = totalPlaces;
+        reservedSeats = 0;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public int getTotalPlaces() {
+        return totalPlaces;
+    }
+
+
+    public int getReservedSeats() {
+        return reservedSeats;
+    }
+
+    private void dateValidation() {
+        LocalDate today = LocalDate.now();
+        if (date.isBefore(today)) {
+            throw new IllegalArgumentException("La data non può essere nel passato.");
+        }
+    }
+
+    private void checkSeats(){
+        if (totalPlaces < 0){
+            throw new IllegalArgumentException("I posti non possono essere negativi.");
+        }
+    }
 }
